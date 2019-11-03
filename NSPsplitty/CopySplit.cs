6bdf39e6098a1e6d8094ad1e29f1bc3e87c6b13a
Aionmagan <57276576+Aionmagan@users.noreply.github.com>
2019-11-03 04:39:36 -0400

    Add files via upload

---
+++ NSPsplitty/CopySplit.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace NSPsplitty
+{
+    public partial class MainWindow
+    {
+        private void SplitCopy()
+        {
+            try
+            {
+                var remSize = fileSize;
+                using (FileStream nspfile = File.OpenRead(fileDir))
+                {
+
+                    for (int i = 0; i < splitNumber + 1; ++i)
+                    {
+                        ConsoleText(string.Format("Starting part {0:00}.", i));
+
+                        var outFile = dir + string.Format("\\{0:00}", i);
+                        using (FileStream splitFile = File.OpenWrite(outFile))
+                        {
+                            long size = 0, tempSize = 0, lastSize = 0, checkSize = splitSize;
+                            byte[] buffer = new byte[chunkSize];
+
+                            if (remSize < splitSize) { checkSize = remSize; }
+                            var checkMB = checkSize / 1024 / 1024;
+
+                            while ((size = (new FileInfo(splitFile.Name).Length)) < checkSize)
+                            {
+                                splitFile.Write(buffer, 0,
+                                                nspfile.Read(buffer, 0, buffer.Length));
+
+                                lastSize = size / 1024 / 1024;
+
+                                if (lastSize <= tempSize) { continue; }
+
+                                tempSize = lastSize;
+                                ConsoleText($" {lastSize}MB / {checkMB}MB", true, ".");
+                            }
+
+                            remSize -= splitSize;
+
+                            splitFile.Flush();
+                            splitFile.Dispose();
+
+                            ConsoleText(string.Format(" Part {0:00} completed", i));
+                        }
+                    }
+
+                    nspfile.Flush();
+                    nspfile.Dispose();
+                }
+
+                ConsoleText("\n NSP splitted sccuessfully");
+
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    Split_Button.IsEnabled = true;
+                }));
+
+            }catch(Exception exception)
+            {
+                ConsoleText(exception.Message);
+                ConsoleText(exception.StackTrace);
+            }
+        }
+    }
+}
---
+++ NSPsplitty/QuickSplit.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace NSPsplitty
+{
+    public partial class MainWindow
+    {
+        private void SplitQuick()
+        {
+            try
+            {
+                ConsoleText(string.Format(" Split is happening from file '{0:00}' to file '01'", splitNumber));
+
+                //moving and rename original file to '00'
+                Directory.Move(fileDir, dir + "\\00");
+
+                var sizeOfSplit = fileSize - (splitSize * splitNumber);
+                var splitNum = splitNumber;
+                var checkMB = (sizeOfSplit / 1024 / 1024);
+
+                long size = 0, tempSize = 0, lastSize = 0;
+                byte[] buffer = new byte[chunkSize];
+
+                //splitting starting from the last file and making it file '01' 
+                //truncating file '00' on the fly
+                using (var nspFile = File.Open(dir + "\\00", FileMode.Open, FileAccess.ReadWrite))
+                {
+                    for (int i = -1; i < splitNumber - 1; ++i)
+                    {
+                        if(i >= 0)
+                        {
+                            sizeOfSplit = splitSize;
+                            splitNum = (splitNumber - (i + 1));
+                            checkMB = (sizeOfSplit / 1024 / 1024);
+                            lastSize = tempSize = 0;
+                        }
+
+                        nspFile.Seek(sizeOfSplit * -1, SeekOrigin.End);
+                        var outFile = dir + string.Format("\\{0:00}", splitNum);
+
+                        ConsoleText(string.Format("Starting part {0:00}.", splitNum));
+                        using (var splitFile = File.OpenWrite(outFile))
+                        {
+                            while ((size = (new FileInfo(splitFile.Name).Length)) < sizeOfSplit)
+                            {
+                                splitFile.Write(buffer, 0,
+                                                nspFile.Read(buffer, 0, buffer.Length));
+                               
+                                lastSize = size / 1024 / 1024;
+                                if (lastSize <= tempSize) { continue; }
+
+                                tempSize = lastSize;
+                                ConsoleText($" {lastSize}MB / {checkMB}MB", true, ".");
+                            }
+
+                            splitFile.Flush();
+                            splitFile.Dispose();
+                        }
+
+                        nspFile.SetLength(nspFile.Seek(sizeOfSplit * -1, SeekOrigin.End));
+                        ConsoleText(string.Format(" Part {0:00} completed", splitNum));
+                    }
+
+                    nspFile.Flush();
+                    nspFile.Dispose();
+                }
+
+                ConsoleText("\n NSP split successfully");
+            }catch(Exception exception)
+            {
+                ConsoleText(exception.Message);
+                ConsoleText(exception.StackTrace);
+            }
+        }
+    }
+}
